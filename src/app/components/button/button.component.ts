cfc436d31281335e4c33ecb2ea8af299c839a400
volodymyrnechyporenko <volodymyrnechyporenko@gmail.com>
2022-02-14 22:15:27 +0200

    Added primary markup for btn and input

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+	selector: 'app-button',
+	templateUrl: './button.component.html',
+	styleUrls: ['./button.component.scss']
+})
+export class ButtonComponent {
+  public buttonText = '';
+
+	@Input()
+	set text(name: string) {
+		this.buttonText = name;
+	}
+	get name(): string {
+		return this.buttonText;
+	}
+
+	@Input() type: string = 'button';
+	@Input() className: string = 'btn_login';
+	@Output() btnClick = new EventEmitter();
+	@Input() isDisabled = false;
+
+	constructor() {}
+
+	onClick() {
+		this.btnClick.emit();
+	}
+}
