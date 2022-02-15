1480d78055ada525ce50651de454ce17ba8ecf13
Illia Diakun <illia_diakun@epam.com>
2022-02-15 17:24:21 +0200

    add module and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,19 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { LoginComponent } from './pages/login/login.component';
-import { ProfileComponent } from './pages/profile/profile.component';
-import { FriendsComponent } from './pages/friends/friends.component';
-import { LibraryComponent } from './pages/library/library.component';
-import { NotfoundComponent } from './pages/notfound/notfound.component';
-import { GamesComponent } from './pages/games/games.component';
-
-const appRoutes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-
-  { path: 'login', component: LoginComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'friends', component: FriendsComponent },
-  { path: 'games', component: GamesComponent },
-  { path: 'library', component: LibraryComponent },
-  { path: '**', component: NotfoundComponent },
-];
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    LoginComponent,
-    ProfileComponent,
-    FriendsComponent,
-    LibraryComponent,
-    NotfoundComponent,
-    GamesComponent,
+    HeaderComponent
   ],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
