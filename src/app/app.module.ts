64cad7cdddbe609c4b72cd533d35d1937756f871
Yulian Khomechko <khomechkoyulian@gmail.com>
2022-02-15 06:39:41 +0200

    routing (header, pages)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { Routes, RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { FriendsComponent } from './pages/friends/friends.component';
+import { LibraryComponent } from './pages/library/library.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { GamesComponent } from './pages/games/games.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  { path: 'login', component: LoginComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'friends', component: FriendsComponent },
+  { path: 'games', component: GamesComponent },
+  { path: 'library', component: LibraryComponent },
+  { path: '**', component: NotfoundComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    ProfileComponent,
+    FriendsComponent,
+    LibraryComponent,
+    NotfoundComponent,
+    GamesComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
