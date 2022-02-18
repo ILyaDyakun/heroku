424f3366d84c18f17f0b9d479acfca6354da44eb
Yulian Khomechko <khomechkoyulian@gmail.com>
2022-02-18 07:33:22 +0200

    search friends is finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,11 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginModule } from './login/login.module';
+import { FriendsComponent } from './friends/friends/friends.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    LoginModule
-  ],
+  declarations: [AppComponent, HeaderComponent, FriendsComponent],
+  imports: [BrowserModule, AppRoutingModule, LoginModule],
   providers: [],
   bootstrap: [AppComponent],
 })
