ef1ecc94347009d0afa06abced865bcbce2aa15c
Illia Diakun <illia_diakun@epam.com>
2022-02-21 22:22:15 +0200

    34567

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginModule } from './login/login.module';
 import { ProfileModule } from './profile/profile.module';
-import { SharedModule } from './shared/shared.module';
-
 import { GamesModule } from './games/games.module';
 import { SharedModule } from './shared/shared.module';
 import { LibraryModule } from './library/library.module';
 import { FriendsModule } from './friends/friends.module';
-
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -20,16 +16,12 @@ import { FriendsModule } from './friends/friends.module';
     BrowserModule,
     AppRoutingModule,
     LoginModule,
-
     ProfileModule,
-    SharedModule
-    FriendsModule,
     SharedModule,
+    FriendsModule,
     GamesModule,
     LibraryModule,
-
   ],
-
   providers: [],
   bootstrap: [AppComponent],
 })
